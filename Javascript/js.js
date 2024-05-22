1c85e14c17b094e9f4f954149d905e40a19af707
Noor Zaqir <nzscode@gmail.com>
2024-05-22 00:40:57 -0400

    2024-05-22

--- Javascript/js.js
+++
@@ -1,2 +0,0 @@
-let inp1 = document.getElementById("jsinput1").addEventListener("click");
-let outpt = document.getElementById("js1-output");
--- raw.js
+++
@@ -1,4 +0,0 @@
-let raw = document.getElementById("raw");
-let p = "papa";
-let coded = document.getElementById("coded");
-console.log(raw.innerHTML.length);
